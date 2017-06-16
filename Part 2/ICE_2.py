504c5ba05307a6844ec7fe6569f3be5e69a2c621
slcc2c <slcc2c@mail.umkc.edu>
2017-06-16 10:52:09 -0500

    Finished ICE 2)

--- Part 2/ICE_2.py
+++ Part 2/ICE_2.py
@@ -7,8 +7,23 @@ def char_freq(word):
         else:
             freqs[l]=1
     return freqs
-word = str(input("What word would you like the frequency of?"))
+word = str(input("What word would you like the frequency of? "))
 freqs = char_freq(word)
 print("In the word ",word,":")
 for k,v in freqs.items():
     print("The letter ",k," has a frequency of ",v)
+
+def longest_word(words):
+    max_length = 0
+    for word in words:
+        if len(word)>max_length:
+            max_length = len(word)
+    return max_length
+def get_words():
+    words = []
+    word = input("Please enter your first word or hit enter on a blank line to exit: ")
+    while(word != ''):
+        words.append(word)
+        word = input("Please enter your another word or hit enter on a blank line to exit: ")
+    return words
+print(longest_word(get_words()))
