a2021150921879db9f9c5a94a6c4f2b021df12f5
Spencer Cook <slcc2c@mail.umkc.edu>
2017-06-22 17:16:03 -0500

    Finished Lab_2 2)
    Function to detect if string contains all letters of alphabet

--- Part 2/Lab_2.py
+++ Part 2/Lab_2.py
@@ -1,3 +1,6 @@
+import string
+
+#Task 1:
 #Write a python program that print the first letter of your name with star. You have to use the instructions in the class.
 # For this you should use nested loop to implement.
 #The expected output should be something like this:
@@ -27,3 +30,19 @@ def ascii_initial():
             print(first[::-1])
         row += 1
 ascii_initial()
+
+#Task 2:
+#Write a Python program to check if a string contains all letters of the alphabet
+
+def entire_alphabet(str):
+	if len(str)<26:
+		return False
+	letters = list(string.ascii_uppercase)
+	for char in str:
+		letters.remove(string.capwords(char))
+		if len(letters)==0:
+			return True
+	return False 
+print(entire_alphabet(string.ascii_lowercase))
+print(entire_alphabet(string.ascii_uppercase))
+print(entire_alphabet("Thiswillreturnfalse"))
