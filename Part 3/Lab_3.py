670f5f49f3b35d144395208d0c79ff78599e6e65
slcc2c <slcc2c@mail.umkc.edu>
2017-06-29 23:19:13 -0500

    Finished 3 Lab 3

--- Part 3/Lab_3.py
+++ Part 3/Lab_3.py
@@ -13,3 +13,52 @@ def listOfTuples():
         user_input = input('enter second number or type q to stop')
         l.append((temp,user_input))
     return sorted(l, key=lambda x:x[1])
+
+#3)
+def print_horiz_line(size):
+    print("--"*size)
+def print_vert_line(size):
+    print("|\t" * size)
+def game_board(size, spaces):
+    for i in range(size):
+        print_horiz_line(size+1)
+        print_vert_line(size+1)
+    print_horiz_line(size+1)
+
+
+def TTT():
+    game_over = False
+    board = []
+    moves = 0;
+    for i in range(9):
+        board.append('-')
+    while(not game_over):
+        print('Move ',moves," Player",(moves%2)+1,"'s turn: ")
+        for i in range(3):
+            print_horiz_line(3)
+            temp = ''
+            for j in range(3):
+                temp += '|'
+                temp += board[(i*3) + j]
+            temp+='|'
+            print(temp)
+        print_horiz_line(3)
+        print('\n')
+
+        valid=False
+        while(not valid):
+            move = input("Player "+str((moves%2)+1)+ " please enter your move as row,col: ")
+            coords = move.split(',')
+            coords[0] = int(coords[0])
+            coords[1] = int(coords[1])
+            if(board[(coords[0]*3)+coords[1]] == '-'):
+                valid = True
+        if((moves%2)==0):
+            board[(coords[0] * 3) + coords[1]] = 'X'
+        else:
+            board[(coords[0] * 3) + coords[1]] = 'O'
+        moves+=1
+        if('-' not in board):
+            game_over = True
+
+TTT()
